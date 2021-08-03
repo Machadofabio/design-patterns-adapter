38557f0a17ec1cb4447aa9db929bdd55fa04f7f4
Fabio Machado <fabio.a.machado@hotmail.com>
2021-08-02 21:07:38 -0300

    Criação de adapter

---
+++ src/vo/Cotacao.java
@@ -0,0 +1,14 @@
+package vo;
+
+public class Cotacao {
+
+    private Double valor;
+
+    public Double getValor() {
+        return valor;
+    }
+
+    public void setValor(Double valor) {
+        this.valor = valor;
+    }
+}
